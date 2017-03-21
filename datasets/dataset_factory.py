043c8a3965b9ac041783664b767320768d3a5b24
yuyang2009 <yuyang20091881@163.com>
2017-03-21 16:49:35 +0800

    ssd

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 from datasets import cifar10
 from datasets import imagenet
+from datasets import fishes
 
 from datasets import pascalvoc_2007
 from datasets import pascalvoc_2012
@@ -29,6 +30,7 @@ datasets_map = {
     'imagenet': imagenet,
     'pascalvoc_2007': pascalvoc_2007,
     'pascalvoc_2012': pascalvoc_2012,
+    'fishes': fishes,
 }
 
 
